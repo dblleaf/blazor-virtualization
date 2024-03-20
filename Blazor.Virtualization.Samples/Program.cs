92069e384c5ed18746ddc141ea8d13871849e94b
Zhaoli Song <song-zhaoli@outlook.com>
2024-03-20 23:23:24 +0800

    first sample

---
+++ Blazor.Virtualization.Samples/Program.cs
@@ -0,0 +1,17 @@
+namespace Blazor.Virtualization.Samples;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        await builder.Build().RunAsync();
+    }
+}
